a8d7b5b5386d769d01dcd17c1a7fd90022b76550
estusflask1177 <128496319+estusflask1177@users.noreply.github.com>
2025-02-26 18:57:55 -0500

    Added Start and Quit Buttons

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
