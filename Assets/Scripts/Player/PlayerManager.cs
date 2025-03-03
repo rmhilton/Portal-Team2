fea0b09295319ca21a3cfbf28abb50ac168d413f
rmhilton <rmhilton@my.waketech.edu>
2025-03-02 23:58:07 -0500

    Fixed some movement code errors

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -1,43 +1,38 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 /*  Raymend started this class to set up the new Input system
- * 
- * 
+ *  Joe continued it to add movement
+ *  Raymend optimized code
  */
 
 public class PlayerManager : MonoBehaviour
 {
     private Vector2 inputDir = Vector2.zero;    //use alongside camera orientation to move player
 
-
     //movement
-//    [SerializeField] private float moveSpeed;
-  //  [SerializeField] private Transform orientation;
+    [SerializeField] private float moveSpeed = 6f;
+    //[SerializeField] private Transform orientation;
 
-    PlayerInput playerInput;
-    InputAction moveAction;
-    InputAction jumpAction;
-
-
-    private void Start()
-    {
-        playerInput = GetComponent<PlayerInput>();
-        moveAction = playerInput.actions.FindAction("Movement");
-        jumpAction = playerInput.actions.FindAction("Jump");
-    }
-
-    private void FixedUpdate()
+    private void Update()
     {
         Move();
     }
-    public void Move(/*InputAction.CallbackContext context*/)
+
+    public void GetInputDir(InputAction.CallbackContext context)
     {
-        Vector2 direction = moveAction.ReadValue<Vector2>();
-        transform.position += new Vector3(direction.x, 0, direction.y) * Time.deltaTime;
+        inputDir = context.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        Vector2 moveDir = inputDir; //multiply inputDir by camera rotation (flattened and normalized) to get movement direction
+
+        transform.position += new Vector3(inputDir.x, 0, inputDir.y) * moveSpeed * Time.deltaTime;
         
         //inputDir = context.ReadValue<Vector2>();
-       // Debug.Log(inputDir);
+        // Debug.Log(inputDir);
     }
 
     public void Jump(InputAction.CallbackContext context)
@@ -46,7 +41,6 @@ public class PlayerManager : MonoBehaviour
         {
             Debug.Log("Jump button pressed!");
         }
-        
     }
 
     public void Interact(InputAction.CallbackContext context)
