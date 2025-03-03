9a8292c1649ded9a1b6694882bad4435a42278fc
rmhilton <rmhilton@my.waketech.edu>
2025-03-03 14:15:31 -0500

    Fixed player movement and added Jump

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -3,21 +3,89 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 /*  Raymend started this class to set up the new Input system
- *  Joe continued it to add movement
- *  Raymend optimized code
+ *  Joe added movement
+ *  Raymend added jump
  */
 
 public class PlayerManager : MonoBehaviour
 {
-    private Vector2 inputDir = Vector2.zero;    //use alongside camera orientation to move player
+    private Vector2 inputDir = Vector2.zero;
 
     //movement
-    [SerializeField] private float moveSpeed = 6f;
-    //[SerializeField] private Transform orientation;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private Transform orientation;
+    private Vector3 moveDir = Vector3.zero;
+    private Rigidbody rb;
+
+    [SerializeField] private float groundDrag = 5f;
+
+    //air movement
+    [SerializeField] private float jumpForce = 12f;
+    [SerializeField] private float jumpCooldown = .25f;
+    [SerializeField] private float airMultiplier = .4f;
+    private bool readyToJump = true;
+
+    //ground check
+    [SerializeField] private float playerHeight = 2f;
+    [SerializeField] private LayerMask groundLayer;
+    bool grounded = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
 
     private void Update()
     {
-        Move();
+        //ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * .5f + 0.2f, groundLayer);
+
+        SpeedControl();
+
+        //handle drag
+        if(grounded)
+        {
+            rb.linearDamping = groundDrag;
+        }
+        else
+        {
+            rb.linearDamping = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+
+        rb.AddForce(Vector3.down * Physics.gravity.magnitude * 2f, ForceMode.Force);
+    }
+
+    private void MovePlayer()
+    {
+        moveDir = orientation.forward * inputDir.y + orientation.right * inputDir.x;
+
+        if(grounded)
+        {
+            rb.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+        else if (!grounded)
+        {
+            rb.AddForce(moveDir.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+        
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        //limit velocity if needed
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.y);
+        }
     }
 
     public void GetInputDir(InputAction.CallbackContext context)
@@ -25,25 +93,35 @@ public class PlayerManager : MonoBehaviour
         inputDir = context.ReadValue<Vector2>();
     }
 
-    private void Move()
+    public void JumpInput(InputAction.CallbackContext context)
     {
-        Vector2 moveDir = inputDir; //multiply inputDir by camera rotation (flattened and normalized) to get movement direction
-
-        transform.position += new Vector3(inputDir.x, 0, inputDir.y) * moveSpeed * Time.deltaTime;
-        
-        //inputDir = context.ReadValue<Vector2>();
-        // Debug.Log(inputDir);
-    }
-
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if(context.phase == InputActionPhase.Started)
+        if(context.phase == InputActionPhase.Performed)
         {
             Debug.Log("Jump button pressed!");
+            if(readyToJump && grounded)
+            {
+                readyToJump = false;
+                Jump();
+
+                Invoke(nameof(ResetJump), jumpCooldown);
+            }
         }
     }
 
-    public void Interact(InputAction.CallbackContext context)
+    private void Jump()
+    {
+        //reset y velocity
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+    public void InteractInput(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Started)
         {
