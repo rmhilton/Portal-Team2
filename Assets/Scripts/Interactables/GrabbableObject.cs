b2f7b13258182ed6a9eddd6911184b7154352f65
rmhilton <rmhilton@my.waketech.edu>
2025-03-05 18:02:07 -0500

    Improved Cube physics

--- Assets/Scripts/Interactables/GrabbableObject.cs
+++ Assets/Scripts/Interactables/GrabbableObject.cs
@@ -9,6 +9,9 @@ public class GrabbableObject : InteractableObject
     //[SerializeField] private float maxMoveSpeed = 5.0f;
     //[SerializeField] private float maxCatchUpDist = 1.2f;
     [SerializeField] private float pickupForce = 150f;
+    [SerializeField] private float releaseForce = 10f;
+    private Vector3 prevPos;
+    
 
     private void Start()
     {
@@ -33,7 +36,7 @@ public class GrabbableObject : InteractableObject
             if(holdGoal)
             {
                 goalPos = holdGoal;
-                transform.parent = goalPos;
+                //transform.parent = goalPos;
             }
         }
         else
@@ -44,7 +47,9 @@ public class GrabbableObject : InteractableObject
             rb.linearDamping = 0f;
             rb.freezeRotation = false;
             goalPos = null;
-            transform.parent = null;
+            //transform.parent = null;
+            Vector3 dropDir = (transform.position - prevPos);
+            //rb.AddForce(dropDir * releaseForce, ForceMode.Impulse);
         }
     }
 
@@ -57,6 +62,7 @@ public class GrabbableObject : InteractableObject
             {
                 Vector3 moveDir = (goalPos.position - transform.position).normalized;
                 rb.AddForce(moveDir * pickupForce);
+                prevPos = transform.position;
             }
         }
     }
